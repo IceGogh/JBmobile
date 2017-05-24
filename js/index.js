635988c2a15597c63499ee094863cbd0ef275700
IceGogh <4535292@qq.com>
2017-05-24 11:44:46 +0800

    recover new code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,62 @@
 
-//  mainBottom nav click
-(function(){
-	var Flag = true;
-	$(function(){
-		$('.mainbottom li > a ').on('click',function(ev){
-			ev = ev || window.event;
-			ev.preventDefault();
-			if(Flag){
-				$(this).css({backgroundImage:' url("img/mainbottom2.png")'})
+//  silde nav click
+
+	var Flag1 = true;
+	sildeNav($('.mainbottom li > a '));
+	sildeNav($('.mubuinner li > a '));
+	
+	function sildeNav(elm){
+		elm.on('click',function(){
+			if(Flag1){
+				$(this).parents('ul').find('.list2nd').slideUp();
+				$(this).css({backgroundImage:' url("img/mainbottom2.png")'});
 				$(this).next().slideDown();
-				Flag = false;
+				Flag1 = false;
 			}else{
-				$(this).css({backgroundImage:' url("img/mainbottom.png")'})
+				$(this).css({backgroundImage:' url("img/mainbottom.png")'});
 			
 				$(this).next().slideUp();
-				Flag = true;
+				Flag1 = true;
 			}
 			
 		})
-	});
-})()
+	
+	}
+	
+
+
+// header mubu offOn
+	var flag2 = true;
+	
+	
+	$('.topMenus span').on('click',function(){
+		var $this = $(this);
+		if(flag2){
+			$('.mubu').slideDown();
+			$this.removeClass('fa-bars').addClass('fa-times');
+			$('.mubu').promise().done(function(){
+				$('.main').css({display:'none'});
+				$('.logo').addClass('mubulogo');
+				$this.css({color:'#fff'});
+				$('.mubuinner li a ').css({color:"rgba(255,255,255,.7)" ,  "transform": "translate(0) scale(1)"});
+				$('.mubuinner > div > a').css({opacity:'1'});
+			})
+			flag2 = !flag2;
+		}else{
+			$('.mubu').slideUp();
+			$('.mubuinner .list2nd').slideUp();
+			$this.removeClass('fa-times').addClass('fa-bars');
+			$('.main').css({display:'block'});
+			$('.mubuinner li a').css({color:"#2f2f2f" , 
+     transform:"translate(75px,-15px) scale(1.2)"})
+			$('.mubu').promise().done(function(){	
+				$('.logo').removeClass('mubulogo');
+				$this.css({color:'inherit'});
+					$('.mubuinner > div > a').css({opacity:0});
+			});
+			flag2 = !flag2;
+		}
+		
+	})
+	
+	
