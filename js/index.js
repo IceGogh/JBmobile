4c3965ff92c6932570163ea665d47ac62c13d16d
IceGogh <4535292@qq.com>
2017-05-25 10:49:01 +0800

    recode the js file

--- js/index.js
+++ js/index.js
@@ -1,41 +1,27 @@
 
 //  silde nav click
-
-	var Flag1 = true;
 	sildeNav($('.mainbottom li > a '));
 	sildeNav($('.mubuinner li > a '));
-	
 	function sildeNav(elm){
 		elm.on('click',function(){
-			// 先归置所有选项为默认状态
-			$(this).parents('ul').find('.list2nd').slideUp();
-			$(this).parents('ul').find('.list2nd').prev().css({backgroundImage:' url("img/mainbottom.png")'});
-			//  哨兵值恢复为 true
-			if(!Flag1){
-				Flag1 = !Flag1;
-			}
 			// 开始判断
-			if(Flag1){
+			if($(this).attr('data-flag') == "true"){
+				$(this).parents('ul').find('.list2nd').slideUp();
+				$(this).parents('ul').find('.list2nd').prev().css({backgroundImage:' url("img/mainbottom.png")'});
 				$(this).css({backgroundImage:' url("img/mainbottom2.png")'});
 				$(this).next().slideDown();
-				Flag1 = false;
-			}else{
-				$(this).css({backgroundImage:' url("img/mainbottom.png")'});
-			
-				$(this).next().slideUp();
-				Flag1 = true;
+				$(this).parents('ul').find('li > a').attr('data-flag','true');
+				$(this).attr('data-flag','false')
 			}
-			
+			else{
+				$(this).css({backgroundImage:' url("img/mainbottom.png")'});
+				$(this).next().slideUp();
+				$(this).attr('data-flag','true')
+			}
 		})
-	
 	}
-	
-
-
 // header mubu offOn
 	var flag2 = true;
-	
-	
 	$('.topMenus span').on('click',function(){
 		var $this = $(this);
 		if(flag2){
@@ -52,10 +38,13 @@
 		}else{
 			$('.mubu').slideUp();
 			$('.mubuinner .list2nd').slideUp();
+			$('.mubuinner li > a ')
+				.attr('data-flag','true')
+				.css({backgroundImage:' url("img/mainbottom.png")'});
 			$this.removeClass('fa-times').addClass('fa-bars');
 			$('.main').css({display:'block'});
 			$('.mubuinner li a').css({color:"#2f2f2f" , 
-     transform:"translate(75px,-15px) scale(1.2)"})
+     transform:"translate(75px,-15px) scale(1.2)"});
 			$('.mubu').promise().done(function(){	
 				$('.logo').removeClass('mubulogo');
 				$this.css({color:'inherit'});
